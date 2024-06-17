82ee13aeec6d905c424a0a73b1a87f63c82c3711
Pierre Gangneux <pierre.gangneux@etu.univ-orleans.fr>
2024-06-17 11:04:21 +0200

    Affichage Page de Connexion

---
+++ IHM/PageConnexion.java
@@ -0,0 +1,27 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class PageConnexion extends Application {
+    @Override
+    public void init(){
+
+    }
+
+
+    @Override
+    public void start(Stage primarystage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("PageConnexion.fxml"));
+        VBox root = loader.load();
+        Scene maiScene = new Scene(root);
+        primarystage.setScene(maiScene);
+        primarystage.show(); 
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
